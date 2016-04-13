5cfe6e3e1260b770a2c0eae7b631fec6b59b8059
Mattia Larentis <mattia@larentis.eu>
2016-04-13 23:43:12 +0200

    fix indent

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,29 +9,29 @@ import WebpackDevServer from 'webpack-dev-server';
 gulp.task('default', ['server']);
 
 gulp.task('babel', () => {
-  return gulp.src('./js/**')
+    return gulp.src('./js/**')
     .pipe(babel())
     .pipe(gulp.dest('target'));
 })
 
 gulp.task('test', ['babel'], () => {
-  return gulp.src('./test/**.js')
+    return gulp.src('./test/**.js')
     .pipe(mocha())
     .on('error', () => {
-      gulp.emit('end');
+        gulp.emit('end');
     });
 });
 
 gulp.task('server', (callback) => {
-  new WebpackDevServer(webpack(webpackConfig), {
-    publicPath: webpackConfig.output.publicPath,
-    hot: true,
-    inline: true,
-    historyApiFallback: true
-  }).listen(3000, '0.0.0.0', (err) => {
-    if (err) {
-      throw new gutil.PluginError('webpack-dev-server', err);
-    }
-    gutil.log('[webpack-dev-server]', 'http://0.0.0.0:3000');
-  });
+    new WebpackDevServer(webpack(webpackConfig), {
+        publicPath: webpackConfig.output.publicPath,
+        hot: true,
+        inline: true,
+        historyApiFallback: true
+    }).listen(3000, '0.0.0.0', (err) => {
+        if (err) {
+            throw new gutil.PluginError('webpack-dev-server', err);
+        }
+        gutil.log('[webpack-dev-server]', 'http://0.0.0.0:3000');
+    });
 });
